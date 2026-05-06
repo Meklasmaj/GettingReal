c4f5865d3ced005bb94a63c2d8d1d2f90a1ffb7b
Mppn <mikkelppnielsen@gmail.com>
2026-05-06 14:26:04 +0200

    Adds IGenericRepo interface

---
+++ LagersystemHansenSeest/Interfaces/IGenericRepo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LagersystemHansenSeest.Interfaces;
+
+// Generic repo interface
+internal interface IGenericRepo<T>
+{
+    T? Get(int id);
+    T? Get(string name);
+    IEnumerable<T> GetAll();
+    void Add(T item);
+    void Update(T item);
+    void Delete(int id);
+    void Delete(string name);
+    IEnumerable<T> Search(string term);
+}
