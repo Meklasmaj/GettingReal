0f527eea5f2f318ede6945f052badc77a0094e6c
Mppn <mikkelppnielsen@gmail.com>
2026-05-06 10:21:42 +0200

    Adds ÆØÅ functionality

--- LagersystemHansenSeest/Program.cs
+++ LagersystemHansenSeest/Program.cs
@@ -1,9 +1,13 @@
-﻿namespace LagersystemHansenSeest;
+﻿using System.Text;
+
+namespace LagersystemHansenSeest;
 
 class Program
 {
     static void Main(string[] args)
     {
+        // To show ÆØÅ in the console
+        Console.OutputEncoding = Encoding.UTF8;
         Console.WriteLine("Hello, World!");
     }
 }
