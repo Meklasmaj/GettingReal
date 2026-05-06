9cb80e631b84ed08c635d6c3ef0263e309b42e8e
Meklasmaj <128758763+Meklasmaj@users.noreply.github.com>
2026-05-06 15:33:24 +0200

    Removing by name and making id strings in IGenericRepo

--- EasyWarehouseManagementSystem.Core/Interfaces/IGenericRepo.cs
+++ EasyWarehouseManagementSystem.Core/Interfaces/IGenericRepo.cs
@@ -7,11 +7,10 @@ namespace EasyWarehouseManagementSystem.Core.Interfaces;
 // Generic repo interface
 internal interface IGenericRepo<T>
 {
-    T? Get(int id);
+    T? Get(string id);
     IEnumerable<T> GetAll();
     void Add(T item);
     void Update(T item);
-    void Delete(int id);
-    void Delete(string name);
+    void Delete(string id);
     IEnumerable<T> Search(string term);
 }
