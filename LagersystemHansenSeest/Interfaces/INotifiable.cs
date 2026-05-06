731a97b7c476307e9ddc4d1580af5be92b788e1a
Mppn <mikkelppnielsen@gmail.com>
2026-05-06 14:17:26 +0200

    Adds interface folder with interface INotifiable

---
+++ LagersystemHansenSeest/Interfaces/INotifiable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LagersystemHansenSeest.Interfaces;
+
+// Interface to generate notifications
+public interface INotifiable
+{
+    bool SetNotification();
+
+    string GetNotification();
+}
