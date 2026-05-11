a8fa0aa6329aac0d07ba210b1d8964e1e5afc69b
Mppn <mikkelppnielsen@gmail.com>
2026-05-11 15:09:39 +0200

    Made SearchEngine.cs

---
+++ EasyWarehouseManagementSystem.Core/Models/SearchEngine.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EasyWarehouseManagementSystem.Core.Models;
+
+namespace EasyWarehouseManagementSystem.Core.Models
+{
+    public static class SearchEngine
+    {
+    }
+}
