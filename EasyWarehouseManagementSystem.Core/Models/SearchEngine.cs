128939edfca289ac9f5516d705d46aa0b70b078f
Mppn <mikkelppnielsen@gmail.com>
2026-05-11 15:21:44 +0200

    Adds method to SearchEngine, to search for Name or ProdutNumber matches

--- EasyWarehouseManagementSystem.Core/Models/SearchEngine.cs
+++ EasyWarehouseManagementSystem.Core/Models/SearchEngine.cs
@@ -7,5 +7,11 @@ namespace EasyWarehouseManagementSystem.Core.Models
 {
     public static class SearchEngine
     {
+        // Searches based on Name OR ProductNumber
+        public static IEnumerable<Product> Search(string term, IEnumerable<Product> products)
+        {
+            term = term.ToLower();
+            return products.Where(p => p.Name.ToLower().Contains(term) || p.ProductNumber.ToLower().Contains(term));
+        }
     }
 }
