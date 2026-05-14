aa46070a1158be88ea1410ee42b35100285a6087
Meklasmaj <128758763+Meklasmaj@users.noreply.github.com>
2026-05-14 13:14:10 +0200

    Creating OrderLine class

---
+++ EasyWarehouseManagementSystem.Core/Models/OrderLine.cs
@@ -0,0 +1,23 @@
+﻿using EasyWarehouseManagementSystem.Core.Interfaces;
+
+namespace EasyWarehouseManagementSystem.Core.Models;
+
+public class OrderLine : IHasId
+{
+    public int Id { get; set; }
+    public int Amount { get; private set; }
+    public Stock? Stock { get; private set; }
+    
+    public OrderLine(){}
+
+    public OrderLine(Stock stock)
+    {
+        Amount = stock.Product.Category.MinOrderAmount;
+        Stock = stock;
+    }
+
+    public override string ToString()
+    {
+        return $"{Stock.Product.Name} | På lager: {Stock.Amount} | Skal bestilles: {Amount}";
+    }
+}
