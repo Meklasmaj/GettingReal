e022c3b50a1546713728b3c77d83ae57dd5c6311
Meklasmaj <128758763+Meklasmaj@users.noreply.github.com>
2026-05-14 13:58:57 +0200

    Recreating main menu, so its ready for next step

--- EasyWarehouseManagementSystem.ConsoleApp/MainMenu.cs
+++ EasyWarehouseManagementSystem.ConsoleApp/MainMenu.cs
@@ -2,8 +2,13 @@
 
 public class MainMenu : Menu
 {
+    private static int _notifications = 0;
+    public MainMenu(int notifications)
+    {
+        _notifications = notifications;
+    }
     // The menu text
-    private static readonly string[] Options = ["Mulighed 1", "Mulighed 2", "Mulighed 3", "Mulighed 4"];
+    private static readonly string[] Options = [$"Kladdeordrer : {_notifications.ToString()}", "Lager", "Produkter", "Leverandører"];
 
     public override void ShowMenu()
     {
@@ -11,17 +16,20 @@ public class MainMenu : Menu
 
         switch (choice)
         {
+            case -1:
+                Environment.Exit(0);
+                break;
             case 1:
-
+                Program.DraftOrderMenu.ShowMenu();
                 break;
             case 2:
-
+                Program.StockMenu.ShowMenu();
                 break;
             case 3:
-
+                Program.ProductMenu.ShowMenu();
                 break;
             case 4:
-
+                Program.SupplierMenu.ShowMenu();
                 break;
         }
     }
